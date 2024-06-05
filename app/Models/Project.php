f69f845748973f47c2d852f1d0a84e03c8453908
Francesca Barletta <wallebar@gmail.com>
2024-06-05 14:55:39 +0200

    ADD: reatioship between projects and type in their models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,11 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Aoo\Models\Type;
 
 class Project extends Model
 {
     use HasFactory;
+
+    public function type(){
+        return $this->belongsTo(Type::class);
+    }
     protected $fillable = [
-        'progetto', 'descrizione', 'link'
+        'progetto', 'descrizione', 'link', 'type_id'
     ];
 }
