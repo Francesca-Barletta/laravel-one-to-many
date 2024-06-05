052d34bb54470cdad897175d64fa5d67758dcd9c
Francesca Barletta <wallebar@gmail.com>
2024-06-05 18:23:40 +0200

    ADD: delete confirm with js modal

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,25 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+
+document.querySelectorAll('.delete-form').forEach(form =>{
+    form.addEventListener('submit', (ev)=>{
+        ev.preventDefault();
+
+        const modalDomElement = form.querySelector('.modal');
+        const modalDomElementYes = form.querySelector('.modal-yes');
+        const modalDomElementNo = form.querySelector('.modal-no');
+
+        modalDomElement.classList.add('visible');
+
+        modalDomElementNo.addEventListener('click', function(){
+            modalDomElement.classList.remove('visible');
+        })
+
+        modalDomElementYes.addEventListener('click', function(){
+
+            form.submit();
+        })
+    })
+})
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,49 +1,55 @@
 @extends('layouts.app')
 @section('content')
-<table class="table">
-<thead>
-    <tr>
-      <th>ID</th>
-      <th>Progetto</th>
-      <th>Slug</th>
-      <th>Descrizione</th>
-      <th>Link alla cartella git-hub</th>
-      <th>Tipologia</th>
-      <th></th>
-      <th></th>
-      <th></th>
-
-      
-
+<div class="index-content position-relative">
+  <table class="table mh-100">
+    <thead>
+        <tr>
+          <th>ID</th>
+          <th>Progetto</th>
+          <th>Slug</th>
+          <th>Descrizione</th>
+          <th>Link alla cartella git-hub</th>
+          <th>Tipologia</th>
+          <th></th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($projects as $project)
+          <tr>
+            <th scope="row">{{ $project->id }}</th>
+            <td>{{ $project->progetto }}</td>
+            <td>{{ $project->slug }}</td>
+            <td>{{ $project->descrizione }}</td>
+            <td><a href="{{ $project->link }}">Link</a></td>
+            <td>{{ optional($project->type)->name }}</td>
+            <td><a href="{{ route('admin.projects.show', $project) }}">Mostra</a></td>
+            <td><a href="{{ route('admin.projects.edit', $project) }}">Modifica</a></td>
+            <td>
+              <form action="{{ route('admin.projects.destroy', $project) }}" method="POST" class="delete-form">
     
-
-    </tr>
-  </thead>
-  <tbody>
-    @foreach ($projects as $project)
-      <tr>
-        <th scope="row">{{ $project->id }}</th>
-        <td>{{ $project->progetto }}</td>
-        <td>{{ $project->slug }}</td>
-        <td>{{ $project->descrizione }}</td>
-        <td><a href="{{ $project->link }}">Link</a></td>
-        <td>{{ optional($project->type)->name }}</td>
-        <td><a href="{{ route('admin.projects.show', $project) }}">Mostra</a></td>
-        <td><a href="{{ route('admin.projects.edit', $project) }}">Modifica</a></td>
-        <td>
-          <form action="{{ route('admin.projects.destroy', $project) }}" method="POST" class="m-0">
-
-            <button class="btn btn-link link-danger">Elimina</button>
-            @csrf
-            @method('DELETE')
+                <button class="btn btn-link link-danger">Elimina</button>
+                @csrf
+                @method('DELETE')
+            
+                <div class="modal">
+                  <div class="modal-box">
+                    <h3 class="text-center">Vuoi eliminare questo progetto?</h3>
+                    <span class="link link-danger modal-yes">Si</span>
+                    <span class="link link-success modal-no">No</span>
+                  </div>
+              </div>
+                
+              </form>
+            </td>
+    
+          </tr>
+            @endforeach
           
-          </form>
-        </td>
-
-      </tr>
-        @endforeach
-
-      </table>
+          </table>
+        
+</div>
 
 
 @endsection
